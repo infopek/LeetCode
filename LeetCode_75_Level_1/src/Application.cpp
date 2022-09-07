d78d33de554aa7214263a5e928a6c44172d2ee24
infopek <10saero01@gmail.com>
2022-09-07 17:11:17 +0200

    Solve daily study plan

--- LeetCode_75_Level_1/src/Application.cpp
+++ LeetCode_75_Level_1/src/Application.cpp
@@ -3,8 +3,10 @@
 
 #include <vector>
 #include <unordered_map>
+#include <map>
 #include <set>
 #include <stack>
+#include <queue>
 
 #include <numeric>
 #include <algorithm>
@@ -589,15 +591,48 @@ int NumIslands(std::vector<std::vector<char>>& grid)
 	return islands;
 }
 
+// -------------	DAY 10	------------- //
+
+/* FIBONACCI NUMBER */
+int Fib(const int n)
+{
+	if (n == 0)
+		return 0;
+
+	int a = 0;
+	int b = 1;
+	int result = a + b;
+
+	int i = 1;
+	while (i < n)
+	{
+		result = a + b;
+		a = b;
+		b = result;
+
+		i++;
+	}
+
+	return result;
+}
+
+/* CLIMBING STAIRS */
+int ClimbStairs(const int n)
+{
+	if (n == 1 || n == 2)
+		return n;
+
+	int* dp = new int[n + 1];
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+		dp[i] = dp[i - 1] + dp[i - 2];
+
+	return dp[n];
+}
+
 int main()
 {
-	std::vector<std::vector<char>> grid =
-	{
-		{ '1', '1', '0', '0', '0' },
-		{ '1', '1', '0', '0', '0' },
-		{ '0', '0', '1', '0', '0' },
-		{ '0', '0', '0', '1', '1' }
-	};
-	
-	std::cout << NumIslands(grid);
+	std::cout << ClimbStairs(6);
 }
