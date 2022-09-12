8c0e23bb7519dbd8254e7621a57b8cf220b55d4d
infopek <10saero01@gmail.com>
2022-09-12 23:08:13 +0200

    Solve problem 64: Minimum Path Sum

--- LeetCode_51_100/src/Application.cpp
+++ LeetCode_51_100/src/Application.cpp
@@ -516,13 +516,33 @@ int UniquePathsWithObstacles(std::vector<std::vector<int>>& obstacleGrid)
 	return dp[rows - 1][cols - 1];
 }
 
-/* PROBLEM 64: MINIMUM PATH SUM*/
+/* PROBLEM 64: MINIMUM PATH SUM */
 int MinPathSum(std::vector<std::vector<int>>& grid)
 {
+	const int rows = grid.size();
+	const int cols = grid[0].size();
 
+	// First row and col
+	for (int i = 1; i < rows; i++)
+		grid[i][0] += grid[i - 1][0];
+	for (int j = 1; j < cols; j++)
+		grid[0][j] += grid[0][j - 1];
+
+	// Rest of grid
+	for (int i = 1; i < rows; i++)
+		for (int j = 1; j < cols; j++)
+			grid[i][j] += std::min(grid[i - 1][j], grid[i][j - 1]);
+
+	return grid[rows - 1][cols - 1];
 }
 
 int main()
 {
-
+	std::vector<std::vector<int>> grid =
+	{
+		{1, 3, 1},
+		{1, 5, 1},
+		{4, 2, 1}
+	};
+	std::cout << MinPathSum(grid);
 }
