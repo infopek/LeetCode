7876be4bfa0168cf7cc3e4c6b5bdc83c7fbc4e57
infopek <10saero01@gmail.com>
2022-09-16 16:59:10 +0200

    Solve problem 70: Climbing Stairs

--- LeetCode_51_100/src/Application.cpp
+++ LeetCode_51_100/src/Application.cpp
@@ -811,6 +811,22 @@ int MySqrt(int x)
 	return std::min(left, right);
 }
 
+/* PROBLEM 70: CLIMBING STAIRS */
+int ClimbStairs(const int n)
+{
+	if (n == 1 || n == 2)
+		return n;
+
+	int* dp = new int[n + 1];
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+		dp[i] = dp[i - 1] + dp[i - 2];
+
+	return dp[n];
+}
+
 int main()
 {
 	for (int i = 0; i < 20; i++)
